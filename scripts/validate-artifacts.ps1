d1f2470f83443a30ad6083ca9820a1d84c0d048e
Orest Yakymiv <akimivorest@gmail.com>
2025-05-16 21:37:59 +0300

    add quotes

--- scripts/validate-artifacts.ps1
+++ scripts/validate-artifacts.ps1
@@ -42,7 +42,7 @@ Write-Output "Validating artifacts"
 $data = Import-Excel -StartRow 3 -Path "$tempFolderPath/$spreadsheetName" -WorksheetName $worksheetName
 
 $computeEstimate = $data | Where-Object {$_.'Service Category' -eq 'Compute'}
-if ($computeEstimate) { 
+if ($computeEstimate) {
     if ($computeEstimate.'Service Category'.Count -eq 1) { 
         if ($computeEstimate.'Region' -ne 'UK West') {
             throw "Unable to verify the VM region - please make sure it set to UK West and try again."
@@ -62,26 +62,26 @@ if ($computeEstimate) {
         
         Write-Output "`u{2705} Checked calculations for the Virtual Machine - OK."
 
-    } else { 
+    } else {
         throw "Unable to verify estimates for the category 'Compute' - more than one VM found. Please make sure that you have only one VM in your calculations and try again."
     }
 
-} else { 
+} else {
     throw "Unable to find estimates for the category 'Compute'. Please make sure that you added your Virtual Machine to the pricing calculation."
 }
 
 $storageEstimate = $data | Where-Object {$_.'Service Category' -eq 'Storage'}
-if ($storageEstimate) { 
-    if ($storageEstimate.'Service Category'.Count -eq 1) { 
-        if ($storageEstimate.'Region' -ne 'UK West') { 
+if ($storageEstimate) {
+    if ($storageEstimate.'Service Category'.Count -eq 1) {
+        if ($storageEstimate.'Region' -ne 'UK West') {
             throw "Unable to verify the VM region - please make sure it set to UK West and try again."
         } 
 
-        if (-not $storageEstimate.Description.Contains("Managed Disks, Premium SSD, LRS Redundancy")) { 
+        if (-not $storageEstimate.Description.Contains("Managed Disks, Premium SSD, LRS Redundancy")) {
             throw "Unable to verify disk type - please make sure that you added a mannaded disk with proper disk type (Premium SSD) and replication type (LRS) to the calculations and try again."
         } 
         
-        if (-not $storageEstimate.Description.Contains("P6")) { 
+        if (-not $storageEstimate.Description.Contains("P6")) {
             throw "Unable to verify data disk size - please make sure that disk size for data disk is set to 64GB and try again. "
         } 
 
@@ -92,18 +92,18 @@ if ($storageEstimate) {
     }
 
 } else { 
-    throw "Unable to find estimates for the category 'Storage. Please make sure that you added your data disk to the pricing calculation."
+    throw "Unable to find estimates for the category 'Storage'. Please make sure that you added your data disk to the pricing calculation."
 }
 
 $networkingEstimate = $data | Where-Object {$_.'Service Category' -eq 'Networking'}
-if ($networkingEstimate) { 
-    if ($networkingEstimate.'Service Category'.Count -eq 1) { 
+if ($networkingEstimate) {
+    if ($networkingEstimate.'Service Category'.Count -eq 1) {
         
-        if ($networkingEstimate.'Service type' -ne 'IP Addresses') { 
+        if ($networkingEstimate.'Service type' -ne 'IP Addresses') {
             throw "Unable to verify Public IP resource - please make sure that you added it to the calculations and try again."
         }
         
-        if ($networkingEstimate.'Region' -ne 'UK West') { 
+        if ($networkingEstimate.'Region' -ne 'UK West') {
             throw "Unable to verify the Public IP region - please make sure it set to UK West and try again."
         } 
 
@@ -114,7 +114,7 @@ if ($networkingEstimate) {
     }
 
 } else { 
-    throw "Unable to find estimates for the category 'Networking. Please make sure that you added your Public IP address to the pricing calculation."
+    throw "Unable to find estimates for the category 'Networking'. Please make sure that you added your Public IP address to the pricing calculation."
 }
 
 Write-Output ""
